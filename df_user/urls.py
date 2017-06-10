e8ff71327d4e5fa8bb44ffd53e3a55ac5d6c3bf3
张磊 <593000965@qq.com>
2017-06-10 11:19:28 +0800

    登陆完成

--- df_user/urls.py
+++ df_user/urls.py
@@ -4,7 +4,8 @@ import views
 urlpatterns = [
     url(r'^register/$',views.register),
     url(r'^login/$',views.login),
-    url(r'center_info/$',views.center_info),
-    url(r'center_order/$',views.center_order),
-    url(r'center_site/$',views.center_siter),
+    url(r'^login_handler/$',views.login_handler),
+    url(r'^center_info/$',views.center_info),
+    url(r'^center_order/$',views.center_order),
+    url(r'^center_site/$',views.center_siter),
 ]
--- df_user/views.py
+++ df_user/views.py
@@ -1,49 +1,32 @@
 #coding=utf-8
-from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
 from models import *
 from hashlib import sha1
 # Create your views here.
 
 def register(request):
     # 判断是否是ajax请求
-    a = request.is_ajax()
-    print a
     if request.is_ajax():
         # 接收GET请求,判断用户名,邮箱是否存在,存在返回1,否则可以写入数据库
         result = request.GET
-
         rname = result.get('name', 0)
-       # print rname
         remail = result.get('email', 0)
         dbname = UserInfo.objects.filter(uname=rname)
         dbemail = UserInfo.objects.filter(uemail=remail)
-        #count = UserInfo.objects.filter(uname=uname).count()
-        #print dbname
-        print dbemail,1
-
-
         if len(dbname) > 0:
-            #print dbname[0].uname
             return JsonResponse({'name':rname})
-            #rname=0
         else:
             rname = 0
             if len(dbemail) > 0:
-                print dbemail, 2
-                print '======'
                 return JsonResponse({'email': remail})
             else:
                 print dbemail, 3
                 remail = 0
                 context = {'title': '注册', 'name': rname, 'email': remail}
-                #return JsonResponse({'email': remail})
-
                 return JsonResponse(context)
-
-
     else:
-
+        # post请求接收
         presult = request.POST
         puser = presult.get('user_name', 0)
         ppasswd = presult.get('pwd', 0)
@@ -62,17 +45,72 @@ def register(request):
             user_name.uemail = pemail
             user_name.save()
             return render(request, 'df_user/register.html')
-
         return render(request, 'df_user/register.html',{'title':'注册'})
 
 
 
-
-
-
-
 def login(request):
-    pass
+    context = {'title': '登陆'}
+    return render(request, 'df_user/login.html', context)
+
+
+def login_handler(request):
+    # 创建post对象
+    post_object = request.POST
+    # 接收用户名,密码和记住用户的值(等于'1'表示勾选)
+    ruser = post_object.get('username')
+    rpasswd = post_object.get('pwd')
+    juser = post_object.get('juser')
+    # 获取数据库中的用户信息
+    dbuser = UserInfo.objects.filter(uname=ruser)
+
+
+    # 获取的密码加密
+    s1 = sha1()
+    s1.update(rpasswd)
+    rpasswd_sha1 = s1.hexdigest()
+    dbpasswd = UserInfo.objects.filter(upasswd=rpasswd_sha1)
+    # 判断输入的信息是否与数据库中的相同,相同返回1,不同返回0
+    if len(dbuser)>0:
+        if dbuser[0].uname == ruser:
+            cuser = 1
+        else:
+            cuser = 0
+
+        # 判断密码
+        if len(dbpasswd) > 0:
+            if rpasswd_sha1 == dbpasswd[0].upasswd:
+                cpasswd = 1
+            else:
+                cpasswd = 0
+
+            # 判断是否记住账户,记住写入cookie
+            reponse = redirect('/user/register/')
+            if juser == '1':
+                reponse.set_cookie('user_name', ruser)
+            else:
+                reponse.delete_cookie('user_name')
+            request.session['user_id'] = dbuser[0].id
+            se = request.session.get('user_id')
+            print se,'se'
+            return reponse
+    else:
+        cuser = 0
+        if len(dbpasswd) > 0:
+            cpasswd = 1
+        else:
+            cpasswd = 0
+        del request.session['user_id']
+        se = request.session.get('user_id')
+        print se, 'se'
+        context = {'cuser': cuser, 'cpasswd': cpasswd, }
+        return render(request, 'df_user/register.html', context)
+
+
+
+
+
+
 
 
 def center_info(request):
